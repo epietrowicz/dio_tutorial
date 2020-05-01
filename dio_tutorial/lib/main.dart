522c663854b32970937fc5061ca41952832dc3e8
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2020-05-01 09:29:47 -0500

    initial commit

---
+++ dio_tutorial/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Dio Form Request Tutorial'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String endPoint = 'http://10.0.2.2:8000/analyze';
+  void _choose() async {
+    File file;
+    file = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      //source: ImageSource.gallery,
+    );
+    if (file != null) {
+      _upload(file);
+      setState(() {});
+    }
+  }
+
+  void _upload(File file) async {
+    String fileName = file.path.split('/').last;
+    print(fileName);
+
+    FormData data = FormData.fromMap({
+      "file": await MultipartFile.fromFile(
+        file.path,
+        filename: fileName,
+      ),
+    });
+
+    Dio dio = new Dio();
+
+    dio.post(endPoint, data: data).then((response) {
+      var jsonResponse = jsonDecode(response.toString());
+      print(jsonResponse);
+    }).catchError((error) => print(error));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          _choose();
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
